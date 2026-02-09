f5714262a4fff7ac4ef38c7dbe3f437d93fdfb28
Miguel Rugerio <miguel.rugerio@outlook.com>
2026-02-09 17:16:11 -0600

    Inicio v0: Fundamentos de OOP - clases, objetos y metodos
    Primer ejemplo del curso: se crea la clase Pato con el metodo volar()
    y una clase Principal que instancia el objeto y lo invoca.
    Demuestra la estructura basica de una clase, creacion de instancias
    y llamada a metodos. Incluye nota sobre NullPointerException.

---
+++ Inicio/src/com/curso/v0/Pato.java
@@ -0,0 +1,9 @@
+package com.curso.v0;
+
+public class Pato {
+	
+	void volar() {
+		System.out.println("Pato vuela");
+	}
+
+}
---
+++ Inicio/src/com/curso/v0/Principal.java
@@ -0,0 +1,18 @@
+package com.curso.v0;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//POO
+		Pato pato1 = new Pato();
+		//pato1 = null;
+		pato1.volar(); //NullPointerException
+		
+		//PRIMITIVOS
+		int x;
+		System.out.println("End Program: "); 
+		
+	}
+
+}
