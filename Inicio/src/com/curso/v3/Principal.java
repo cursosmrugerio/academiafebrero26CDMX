a1d5fb3250f9f10302a17f78eafe98f152b6a486
Miguel Rugerio <miguel.rugerio@outlook.com>
2026-02-10 14:20:45 -0600

    Agregar Inicio v3: Hello World con estructura clasica de Java
    Programa basico que demuestra estructura de clase, paquetes,
    creacion de objetos String con new y salida por consola con
    System.out.println.

---
+++ Inicio/src/com/curso/v3/Principal.java
@@ -0,0 +1,14 @@
+package com.curso.v3;
+
+import java.lang.System;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		String hello = new String("Hola mundo");
+		
+		System.out.println(hello);
+	}
+
+}
