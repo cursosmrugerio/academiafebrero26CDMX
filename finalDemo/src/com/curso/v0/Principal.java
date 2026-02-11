c655dc2f90bd1f9a73ffe8cc3508bc0fb01512c6
Miguel Rugerio <miguel.rugerio@outlook.com>
2026-02-11 17:26:38 -0600

    Agregar proyecto finalDemo: modificador final en variables y referencias mutables

---
+++ finalDemo/src/com/curso/v0/Principal.java
@@ -0,0 +1,17 @@
+package com.curso.v0;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		final StringBuilder sb = new StringBuilder("Hello");
+		sb.append(" World");
+		//sb = sb.append(" World");
+		//sb = null;
+		//sb = new StringBuilder("Hola");
+		System.out.println(sb);
+		
+		
+	}
+
+}
