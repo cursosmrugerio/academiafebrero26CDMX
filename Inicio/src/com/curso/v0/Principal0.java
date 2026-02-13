95a2b4379f7b97158b76efee63fd274c9ca35c3e
Miguel Rugerio <miguel.rugerio@outlook.com>
2026-02-13 12:23:56 -0600

    Uso PrintStream

---
+++ Inicio/src/com/curso/v0/Principal0.java
@@ -0,0 +1,15 @@
+package com.curso.v0;
+
+import java.io.PrintStream;
+
+public class Principal0 {
+	
+	public static void main(String[] args) {
+		
+		PrintStream ps = System.out;
+		
+		ps.println("Hola Mundo Academy Java");
+		
+	}
+
+}
